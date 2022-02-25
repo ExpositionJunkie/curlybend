3f77623098d81c02ac37e8055f6bc1004906b90c
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-02-24 17:01:13 -0800

    middleware added

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const hostname = "localhost";
 const port = 4500; //3000 reserved for front end dev
 
 const app = express();
 app.disable("x-powered-by"); //Don't like people to see what technology my backend is running. That's why we hide it!
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-  console.log(req.headers);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/html");
   res.end(
