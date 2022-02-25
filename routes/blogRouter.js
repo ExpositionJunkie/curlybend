d7d96829ebdabbd40f2c2aedd681ab95e41487f8
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-02-24 18:44:29 -0800

    added blog/:blogId boilerplate code

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -26,4 +26,28 @@ blogRouter
     res.end("Deleting all blog entries! May you go with God.");
   });
 
+blogRouter
+  .route("/:blogId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end(`Will send details of the blog: ${req.params.blogId} to you`);
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /blogs/${req.params.blogId}`);
+  })
+  .put((req, res) => {
+    res.write(`Updating the blog: ${req.params.blogId}\n`);
+    res.end(
+      `Will update the blog with the title: ${req.body.title} with text: ${req.body.text}`
+    );
+  })
+  .delete((req, res) => {
+    res.end(`Deleting blog: ${req.params.blogId}`);
+  });
+
 module.exports = blogRouter;
