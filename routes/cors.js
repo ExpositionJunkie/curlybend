4d1da127246c2e6fc2110613728d7158b3dab9e2
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-04 18:13:52 -0800

    minor updates not working correctly yet though.

--- routes/cors.js
+++ routes/cors.js
@@ -1,9 +1,17 @@
 const cors = require("cors");
 
-const whitelist = ["http://localhost:3006", "https://localhost:3449", "https://localhost:3000", "http://curlybrackets.me", "https://curlybrackets.me"];
+const whitelist = [
+  "http://localhost:3006",
+  "https://localhost:3449",
+  "https://localhost:3001",
+  "https://localhost:3000",
+  "http://localhost:3000",
+  "http://curlybrackets.me",
+  "https://curlybrackets.me",
+  "http://192.168.1.26:3000",
+];
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
-  console.log(req.header("Origin"));
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
     corsOptions = { origin: true };
   } else {
--- routes/users.js
+++ routes/users.js
@@ -3,16 +3,18 @@ const User = require("../models/user");
 const passport = require("passport");
 const router = express.Router();
 const authenticate = require("../authenticate");
-const cors = require('./cors')
+const cors = require("./cors");
 
 /* GET users listing. */
-router.route('/')
+router
+  .route("/")
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.cors, (req, res, next) => {
     res.send("respond with a resource");
   });
 
-router.route('/signup')
+router
+  .route("/signup")
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .post(cors.corsWithOptions, (req, res) => {
     User.register(
@@ -48,31 +50,33 @@ router.route('/signup')
     );
   });
 
-router.route('/login')
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.post(cors.corsWithOptions, passport.authenticate("local"), (req, res) => {
-  const token = authenticate.getToken({ _id: req.user._id });
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({
-    success: true,
-    token: token,
-    status: "You are successfully logged in!",
+router
+  .route("/login")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .post(cors.corsWithOptions, passport.authenticate("local"), (req, res) => {
+    const token = authenticate.getToken({ _id: req.user._id });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.json({
+      success: true,
+      token: token,
+      status: "You are successfully logged in!",
+    });
   });
-});
 
-router.route('/logout')
-.options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-.get(cors.cors, (req, res, next) => {
-  if (req.session) {
-    req.session.destroy();
-    res.clearCookie("session-id");
-    res.redirect("/");
-  } else {
-    const err = new Error("You are not logged in!");
-    err.status = 401;
-    return next(err);
-  }
-});
+router
+  .route("/logout")
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
+    if (req.session) {
+      req.session.destroy();
+      res.clearCookie("session-id");
+      res.redirect("/");
+    } else {
+      const err = new Error("You are not logged in!");
+      err.status = 401;
+      return next(err);
+    }
+  });
 
 module.exports = router;
