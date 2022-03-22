02eedf0f56f3695ecea3123422dcfd523f4113cf
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-22 14:40:22 -0700

    updated whitelist

--- routes/cors.js
+++ routes/cors.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const whitelist = [
   "https://curlybrackets.me",
   "192.168.1.42",
-  "fe80::59ae:bbcc:aa50:aeab%15"
+  "fe80::59ae:bbcc:aa50:aeab%15",
+  "http://localhost:3000", 
+  "https://localhost:3443",
 ];
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
