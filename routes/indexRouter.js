324a12ecf21ee3ec5e9dd666a935387cd8128cec
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-22 17:29:17 -0700

    trying cors on index router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const indexRouter = express.Router();
+const cors = require('./cors')
 
 indexRouter
   .route("/")
-  .options((req, res, next) => res.sendStatus(200))
-  .get((req, res, next) => {
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.cors, (req, res, next) => {
     res.statusCode = 200;
     res.render("index", {title: "Curlybooty"})
   })
-  .post((req, res, next) => {
+  .post(cors.cors, (req, res, next) => {
     res.statusCode = 403;
     res.end("POST is not permitted on index.");
   })
-  .put(
+  .put(cors.cors,
     (req, res, next) => {
       res.statusCode = 403;
       res.end("PUT operation not supported on index");
     }
   )
-  .delete((req, res, next) => {
+  .delete(cors.cors, (req, res, next) => {
     res.statusCode = 403;
     res.end("DELETE operation not supported on index");
   });
