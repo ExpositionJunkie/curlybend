8c52ff8d05573650f63283f89aa5f946f8fdc231
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-22 17:38:52 -0700

    updated logger

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ connect
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(logger("prod"));
+app.use(logger("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
