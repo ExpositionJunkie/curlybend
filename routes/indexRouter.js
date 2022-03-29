ddbeabab1d5176a1fed759bf6b22d8963fbcc4e4
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-03-28 20:44:20 -0700

    updated some of the pug styling and front page to be a little more professional.

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,7 +7,7 @@ indexRouter
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.cors, (req, res, next) => {
     res.statusCode = 200;
-    res.render("index", {title: "Api for Curlybrackets.me", message: "Hello and welcome to api.curlybrackets.me. This api serves as an endpoint for data and is protected with authentication measures to provide users a way to update blogs and other information once they are signed up. Documentation at this time is limited due to the fact that this is a private API. More information will be provided should this API be made more public. Thank you for stopping by."})
+    res.render("index", {title: "Api for Curlybrackets.me", message: ["Hello and welcome to api.curlybrackets.me. This api serves as an endpoint for data and is protected with authentication measures to provide users a way to update blogs and other information once they are signed up.","\n Documentation at this time is limited due to the fact that this is a private API. More information will be provided should this API be made more public.","\nThank you for stopping by."]})
   })
   .post(cors.cors, (req, res, next) => {
     res.statusCode = 403;
