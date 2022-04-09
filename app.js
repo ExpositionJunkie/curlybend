321352be580ad1256dd645aef602670572486c43
Roz Albrecht <rosalindavalbrecht@gmail.com>
2022-04-08 19:14:05 -0700

    fixed cors processing

--- app.js
+++ app.js
@@ -8,20 +8,12 @@ const cors = require("./routes/cors")
 
 //sessions and cookie parser - necessary for passport to work
 var session = require("express-session")
-const cookieParser = require("cookie-parser")
 
 //Routes
 const indexRouter = require("./routes/indexRouter");
 const blogRouter = require("./routes/blogRouter");
 const usersRouter = require("./routes/usersRouter");
 const uploadRouter = require("./routes/uploadRouter");
-
-
-//a word on passport and how it works
-//https://www.airpair.com/express/posts/expressjs-and-passportjs-sessions-deep-dive
-//This is why I have the below config as we are using express sessions 4
-//https://mianlabs.com/2018/05/22/using-mongodb-for-quick-session-storage-with-connect-mongo-in-express/
-//This may likewise be the answer
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
@@ -30,19 +22,6 @@ if (process.env.NODE_ENV !== "production") {
 
 //Back to express
 var app = express();
-
-// var postAuth = function (req, res, next) {
-//   // res.setHeader("Access-Control-Allow-Origin", "*");
-//   // res.setHeader("Access-Control-Allow-Credentials", "true");
-//   // res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-
-//   next();
-// };
-
 
 
 //Mongo
--- routes/cors.js
+++ routes/cors.js
@@ -1,25 +1,22 @@
 const cors = require("cors");
 
-const whitelist = ["https://localohost:8080", "curlybrackets.me"];
-
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
   console.log(req.header("Origin"));
 
-  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+
     corsOptions = {
-      origin: true,
+      origin: "*",
       preflightContinue: true,
       allowedHeaders: "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
       methods: "GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH"
     };
-  }else {
-    corsOptions = { origin: false };
-  }
+
+
   callback(null, corsOptions);
 };
 
-exports.cors = cors();
+exports.cors = cors({origin: "*",preflightContinue:true});
 exports.corsWithOptions = cors(corsOptionsDelegate);
 
       // allowedHeaders: ["Content-Type", "application/json"],
